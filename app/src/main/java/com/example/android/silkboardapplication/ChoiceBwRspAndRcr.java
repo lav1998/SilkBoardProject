b51af7fb12cb3cd7369e206c24646b6a0a8ba3cd
Lav Gabri <lav1998@gmail.com>
2018-08-22 00:52:54 +0530

    Almost completed with RCR proforma.

---
+++ app/src/main/java/com/example/android/silkboardapplication/ChoiceBwRspAndRcr.java
@@ -0,0 +1,13 @@
+package com.example.android.silkboardapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoiceBwRspAndRcr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice_bw_rsp_and_rcr);
+    }
+}
