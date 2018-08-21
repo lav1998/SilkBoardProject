a36c02b1cab199fb8c1e0fbba29bfb23de0c441d
Akshay <akshayrajareddy6@gmail.com>
2018-08-21 19:41:36 +0530

    Updated officer's attributes - 1st iteration

--- app/src/main/java/com/example/android/silkboardapplication/SO1.java
+++ app/src/main/java/com/example/android/silkboardapplication/SO1.java
@@ -4,17 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    public void buttonClickFunction(View v)
+public class SO1 extends AppCompatActivity {
+    public void fun(View v)
     {
-        Intent intent = new Intent(getApplicationContext(), SO1.class);
+        Intent intent = new Intent(getApplicationContext(),SO2.class);
         startActivity(intent);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_so1);
     }
 }
